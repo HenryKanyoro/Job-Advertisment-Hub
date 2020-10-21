92099f8a3d6538f7951e13c88bf9171d678ce19a
kiarie <denniskiarie426@gmail.com>
2020-10-21 14:57:09 +0300

    add hover effect

---
+++ js/aboutUs.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $(".floatcaption").hover(function(){
+        $(this).find(".caption").toggle(400);
+    })
+});
+
+// $(document).ready(function() {
+//     $(".photo").hover(function(){
+//         $("#caption").toggle(); 
+// });
+// });
